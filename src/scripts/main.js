c93c2af6c220e4e56535101fb84454a8396da610
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-07-15 16:43:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('li')];
+const list = document.querySelector('ul');
+
+function sortList(list) {
+  return list.sort((a, b) => getNumber(b.dataset.salary) - getNumber(a.dataset.salary));
+}
+
+function getNumber(str) {
+  return str.replace(/[^\d.-]/g, '');
+}
+
+function getEmployees(list) {
+  const result = [];
+
+  list.forEach(el => result.push(
+    {
+      name: el.innerText,
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    }));
+
+    return result
+}
+
+const arrayOfRightObjects = getEmployees(listItems);
+
+const sortedList = sortList(listItems);
+
+for (const li of sortedList) {
+  list.appendChild(li);
+}
+
+
