616be15d636dfc9af8621d0127bf914389c18905
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-07-15 16:54:31 +0300

    fix getNember function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,36 @@
 'use strict';
 
 const listItems = [...document.querySelectorAll('li')];
-const list = document.querySelector('ul');
+const listUl = document.querySelector('ul');
 
 function sortList(list) {
-  return list.sort((a, b) => getNumber(b.dataset.salary) - getNumber(a.dataset.salary));
+  return list.sort(
+    (a, b) => getNumber(b.dataset.salary) - getNumber(a.dataset.salary),
+  );
 }
 
 function getNumber(str) {
-  return str.replace(/[^\d.-]/g, '');
+  return Number(str.replace(/[^\d.-]/g, ''));
 }
 
 function getEmployees(list) {
   const result = [];
 
-  list.forEach(el => result.push(
-    {
+  list.forEach((el) =>
+    result.push({
       name: el.innerText,
       position: el.dataset.position,
       salary: el.dataset.salary,
       age: el.dataset.age,
     }));
 
-    return result
+  return result;
 }
 
-const arrayOfRightObjects = getEmployees(listItems);
+getEmployees(listItems);
 
 const sortedList = sortList(listItems);
 
 for (const li of sortedList) {
-  list.appendChild(li);
+  listUl.appendChild(li);
 }
-
-
